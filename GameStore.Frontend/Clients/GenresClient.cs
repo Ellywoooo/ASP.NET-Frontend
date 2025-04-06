c407c4fc23b5f35ea8e530a81490c12c901d969e
30033719@student.toiohomai.ac.nz <30033719@student.toiohomai.ac.nz>
2025-04-06 18:01:54 +1200

    first commit

---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,8 @@
+using System;
+using GameStore.Frontend.Models;
+using GameStore.Frontend.Clients;
+public class GenresClient(HttpClient httpClient)
+{
+    public async Task<Genre[]> GetGenresAsync() 
+        => await httpClient.GetFromJsonAsync<Genre[]>("genres") ?? [];
+}
